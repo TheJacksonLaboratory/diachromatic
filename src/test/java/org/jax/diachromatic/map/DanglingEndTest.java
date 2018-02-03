fcb3ddef09c0218f89c3682721906f0e5dd1866b
pnrobinson <peter.robinson@charite.de>
2018-02-03 14:38:24 -0500

    documentation

---
+++ src/test/java/org/jax/diachromatic/map/DanglingEndTest.java
@@ -0,0 +1,15 @@
+package org.jax.diachromatic.map;
+
+/** This test class is intended to test whether we can recognize dangling ends.
+ * It is also possible for non-ligated DNA fragments to insert between sequencing
+ * adapters, despite the protocol being designed to minimise such events. Consequently,
+ * HiCUP identifies and removes these unwanted species by checking if the forward and
+ * reverse reads of a di-tag map to the same genomic restriction fragment, but unlike
+ * circularised fragments the reads are orientated towards each other. Furthermore,
+ * HiCUP divides this category into two sub-categories depending on whether the DNA fragment
+ * end overlaps a restriction fragment cut site.
+ * If the fragment end does overlap it is termed a "dangling end" (Figure 2d),
+ * but if it does not it is termed an "internal fragment" (Figure 2e).
+ */
+public class DanglingEndTest {
+}
