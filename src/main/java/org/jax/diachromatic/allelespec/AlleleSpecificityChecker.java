26b21beb7d680e41a5fdb3806ed44a4b82dea76c
pnrobinson <peter.robinson@jax.org>
2019-01-13 11:51:39 -0500

    Prototype for allele counting

--- src/main/java/org/jax/diachromatic/allelespec/AlleleSpecificityChecker.java
+++ src/main/java/org/jax/diachromatic/allelespec/AlleleSpecificityChecker.java
@@ -1,17 +1,15 @@
 package org.jax.diachromatic.allelespec;
 
-import com.google.common.collect.ImmutableList;
 import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.SamReader;
 import htsjdk.samtools.SamReaderFactory;
 import htsjdk.samtools.ValidationStringency;
-import htsjdk.samtools.filter.SamRecordFilter;
-import htsjdk.samtools.util.Interval;
-import htsjdk.samtools.util.IntervalList;
 import htsjdk.samtools.util.SamLocusIterator;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AlleleSpecificityChecker {
@@ -20,6 +18,9 @@ public class AlleleSpecificityChecker {
     private final String bamFilePath;
     /** SAM Reader corresponding to {@link #bamFilePath}. */
     private final SamReader samReader;
+
+
+    private final List<AlleleSpecificLocus> locuslist;
 
 
     /**
@@ -32,77 +33,95 @@ public class AlleleSpecificityChecker {
         SamReaderFactory srf=SamReaderFactory.make();
         srf.validationStringency(ValidationStringency.LENIENT);
         this.samReader= srf.open(new File(bamPath));
+        locuslist=new ArrayList<>();
     }
 
 
     public void readAlleles() {
         SAMFileHeader header = samReader.getFileHeader();
-        IntervalList il = new IntervalList(header);
-        String chrom= "chrY";
-        int from= 1;
-        int to= 100;
+        //IntervalList il = new IntervalList(header);
         //debugPrintHeader(header);
-        List<SamRecordFilter> filter = ImmutableList.of(); // ?? TODO how to do this and do we want filters at all?
-        SamLocusIterator samLocIter= new SamLocusIterator(samReader, il, true);
-        samLocIter = new SamLocusIterator(samReader);
+       // List<SamRecordFilter> filter = ImmutableList.of(); // ?? TODO how to do this and do we want filters at all?
+        SamLocusIterator samLocIter = new SamLocusIterator(samReader);
         int count=0;
         int n_empty=0;
         while (samLocIter.hasNext()) {
             SamLocusIterator.LocusInfo locus = samLocIter.next();
+            int idx =locus.getSequenceIndex();
+            int pos = locus.getPosition();
+            //String ref = samReader.getFileHeader().getSequence(idx).
             if (locus.isEmpty()) {
                 n_empty++;
                 continue; // no reads at this position
             }
             //if (locus.getDeletedInRecord().size()<1) continue;
-            debugPrintLocus(locus);
+            checkLocus(locus);
             List<SamLocusIterator.RecordAndOffset> recordAndOffsetList = locus.getRecordAndOffsets();
             for (SamLocusIterator.RecordAndOffset rao : recordAndOffsetList) {
                // System.out.println(rao.getBaseQualities());
             }
-            if (count++>100)break;
         }
         System.out.println("We found " + n_empty + " empty positions");
     }
 
 
-    private String getBaseDistribution(List<SamLocusIterator.RecordAndOffset> recordAndOffsetList){
+    private boolean checkBaseDistribution(SamLocusIterator.LocusInfo locus ){
+        List<SamLocusIterator.RecordAndOffset> recordAndOffsetList = locus.getRecordAndOffsets();
         int A_count=0,C_count=0,G_count=0,T_count=0;
         final int QUALITY_THRESHOLD=10;
         final byte A_BASE=65;
         final byte C_BASE=67;
         final byte G_BASE=71;
         final byte T_BASE=84;
+        Integer[] counts = new Integer[4];
+        int total=0;
         for (SamLocusIterator.RecordAndOffset rao : recordAndOffsetList) {
             if (rao.getBaseQuality() > QUALITY_THRESHOLD) {
-
                 switch (rao.getReadBase()) {
                     case A_BASE: A_count++;break;
                     case C_BASE: C_count++;break;
                     case G_BASE: G_count++;break;
                     case T_BASE: T_count++;break;
                     default:
+                        // should never happen, but if it does happen there is a bad bug
                         System.out.println(rao.getReadBase() + "="+(char)rao.getReadBase());
                         System.exit(1);
                 }
+                total++;
             }
         }
-        return String.format("A:%d C:%d G:%d T:%d",A_count,C_count,G_count,T_count);
+        //return String.format("A:%d C:%d G:%d T:%d",A_count,C_count,G_count,T_count);
+       // System.err.println(String.format("totla=%d // A:%d C:%d G:%d T:%d",total,A_count,C_count,G_count,T_count));
+        if (A_count==total || C_count==total || G_count==total || T_count==total) {
+           // System.err.println("returning false");
+            return false; // homozygous! No allele bias possible
+        } else if (total>0){
+            counts[0]=A_count;counts[1]=C_count;counts[2]=C_count;counts[3]=T_count;
+            Arrays.sort(counts,Collections.reverseOrder()); // or just take from other end
+            AlleleSpecificLocus asl = new AlleleSpecificLocus(locus.getSequenceName(),locus.getPosition(),total,counts[0],counts[1]);
+            this.locuslist.add(asl);
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private void debugPrintLocus(SamLocusIterator.LocusInfo locus) {
-        System.out.println("locus:");
-        System.out.println("\tname: "+locus.getSequenceName());
-        System.out.println("\tpos: "+locus.getPosition());
-        System.out.println("\tsize: "+locus.size());
-        System.out.println("\tisEmpty?: "+locus.isEmpty());
-        System.out.println("\tsequence length: "+locus.getSequenceLength());
-        System.out.println("\tsequence index: "+locus.getSequenceIndex());
-        System.out.println("\ttostring: "+locus.toString());
-        List<SamLocusIterator.RecordAndOffset> recordAndOffsetList =locus.getRecordAndOffsets();
-        System.out.println("\trecords and offsets: ");
-        System.out.println(getBaseDistribution(recordAndOffsetList));
+    private void checkLocus(SamLocusIterator.LocusInfo locus) {
+        // first check for single-nucleotide variants
+        if (checkBaseDistribution(locus)) {
+            System.out.println("locus:");
+            System.out.println("\tname: "+locus.getSequenceName());
+            System.out.println("\tpos: "+locus.getPosition());
+            System.out.println("\tsize: "+locus.size());
+            System.out.println("\tisEmpty?: "+locus.isEmpty());
+            System.out.println("\tsequence length: "+locus.getSequenceLength());
+            System.out.println("\tsequence index: "+locus.getSequenceIndex());
+            System.out.println("\ttostring: "+locus.toString());
+            System.out.println();
+        }
+
         List<SamLocusIterator.RecordAndOffset> deleted=locus.getDeletedInRecord();
-        debugPrintDeleted(deleted);
+        //debugPrintDeleted(deleted);
         return;/*
         for (SamLocusIterator.RecordAndOffset rao : recordAndOffsetList) {
             System.out.println("\treadname: "+rao.getReadName());
@@ -142,10 +161,13 @@ public class AlleleSpecificityChecker {
         //return String.format("A:%d C:%d G:%d T:%d",A_count,C_count,G_count,T_count);
     }
 
+    /** Only used for development, can be deleted. */
     private void debugPrintHeader(SAMFileHeader header) {
         // printout entire SAM File header section.
         System.out.println(header.getSAMString());
     }
 
-
+    public List<AlleleSpecificLocus> getLocuslist() {
+        return locuslist;
+    }
 }
--- src/test/java/org/jax/diachromatic/allelespec/AlleleSpecificityCheckerTest.java
+++ src/test/java/org/jax/diachromatic/allelespec/AlleleSpecificityCheckerTest.java
@@ -1,16 +1,38 @@
 package org.jax.diachromatic.allelespec;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class AlleleSpecificityCheckerTest{
-    /** Just to get started, a local file. */
-    private static String bamPath="/Users/peterrobinson/Documents/data/diachromatic/HG00158_chr1_chr22_toy_example.bam";
+    /** Path to BAM file created with help of {@link MakeFilesForTest}. */
+    private static String bamPath;
+
+    @BeforeClass
+    public static void init() throws FileNotFoundException {
+        ClassLoader classLoader = AlleleSpecificityCheckerTest.class.getClassLoader();
+        URL resource = classLoader.getResource("data/allelespec/sample.sorted.bam");
+        if (resource==null){
+            throw new FileNotFoundException("Could not find sample.sorted.bam file");
+        }
+        bamPath = resource.getFile();
+    }
 
 
     @Test
     public void test1() {
         AlleleSpecificityChecker checker = new AlleleSpecificityChecker(bamPath,"");
         checker.readAlleles();
+        assertEquals(2,checker.getLocuslist().size());
+        for (AlleleSpecificLocus loc : checker.getLocuslist()) {
+            System.err.println(loc);
+            System.err.println("p="+loc.binomialTestPvalue());
+        }
     }
 
 
