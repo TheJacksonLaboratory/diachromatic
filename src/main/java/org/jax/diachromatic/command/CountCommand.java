d309da9f16c6e2804bf9c7a172746a3eabbbbb45
pnrobinson <peter.robinson@jax.org>
2019-02-12 11:22:11 -0500

    JCommander

--- src/main/java/org/jax/diachromatic/command/CountCommand.java
+++ src/main/java/org/jax/diachromatic/command/CountCommand.java
@@ -1,5 +1,7 @@
 package org.jax.diachromatic.command;
 
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jax.diachromatic.align.DigestMap;
@@ -7,26 +9,26 @@ import org.jax.diachromatic.count.Counter;
 import org.jax.diachromatic.exception.DiachromaticException;
 
 import java.io.FileNotFoundException;
-
+@Parameters(commandDescription = "count TODO-more text")
 public class CountCommand extends Command {
     private static final Logger logger = LogManager.getLogger();
 
     /** Path to BAM file containing unique valid pairs. */
+    @Parameter(names={"-v", "--valid-pairs-bam"},required = true,description = "path to BAM file with unique valid pairs produced using the 'align' subcommand")
     private String validPairsBamFile = null;
 
     /** Path to the genome digest file produced by GOPHER. */
+    @Parameter(names={"-d","--digest-file"}, required = true,description = "path to GOPHER digest file")
     private String digestFile = null;
 
-    private String outputPathPrefix = null;
-
+    @Parameter(names={"-o", "--out"},required = true,description = "name/path of output file/directory")
+    private String outputPathPrefix;
+    //TODO -- can we chance to -x, --prefix ?
+    @Parameter(names={"-op", "--out-prefix"},required = true,description = "outprefix for files in output directory")
     private String filenamePrefix;
 
 
-    public CountCommand(String validPairsBamFile, String digestFile, String outputPathPrefix, String filenamePrefix) {
-        this.validPairsBamFile = validPairsBamFile;
-        this.digestFile = digestFile;
-        this.outputPathPrefix = outputPathPrefix;
-        this.filenamePrefix = filenamePrefix;
+    public CountCommand() {
     }
 
     public void execute() throws DiachromaticException {
