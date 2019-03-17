5eda2350a6f61afb9460740d42c27ac466121dec
pnrobinson <peter.robinson@jax.org>
2019-03-17 13:54:25 -0400

    improving command-line error messages

--- src/main/java/org/jax/diachromatic/Diachromatic.java
+++ src/main/java/org/jax/diachromatic/Diachromatic.java
@@ -4,6 +4,7 @@ package org.jax.diachromatic;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.common.collect.ImmutableSet;
 import org.jax.diachromatic.command.*;
 import org.jax.diachromatic.exception.DiachromaticException;
 import org.apache.logging.log4j.LogManager;
@@ -12,6 +13,7 @@ import org.apache.logging.log4j.Logger;
 /**
  * An application to process Hi-C data for differential reads counts in fragments surrounding the
  * transcription start site using probe design by VPV.
+ *
  * @author <a href="mailto:peter.robinson@jax.org">Peter Robinson</a>
  * @author <a href="mailto:peter.hansen@charite.de">Peter Hansen</a>
  * @version 0.0.1 (2017-11-15)
@@ -19,8 +21,10 @@ import org.apache.logging.log4j.Logger;
 public class Diachromatic {
     private static final Logger logger = LogManager.getLogger();
 
-    @Parameter(names = {"-h", "--help"}, help = true, arity = 0,description = "display this help message")
+    @Parameter(names = {"-h", "--help"}, help = true, arity = 0, description = "display this help message")
     private boolean usageHelpRequested;
+
+    private static final ImmutableSet<String> commandnames = ImmutableSet.of("truncate", "align", "count", "summarize");
 
 
     public static void main(String[] args) throws DiachromaticException {
@@ -45,28 +49,44 @@ public class Diachromatic {
             // Note that by default, JCommand is OK with -h download but
             // not with download -h
             // The following hack makes things work with either option.
-            String commandString=null;
-            for (String a:args) {
-//                if (commandnames.contains(a)) {
-//                    commandString=a;
-//                }
-                if (a.contains("h")) {
-                    if (commandString!=null) {
-                        jc.usage(commandString);
+            String mycommand = null;
+            String commandstring = String.join(" ", args);
+            for (String a : args) {
+                if (commandnames.contains(a)) {
+                    mycommand = a;
+                }
+                if (a.equals("h")) {
+                    if (mycommand != null) {
+                        jc.usage(mycommand);
                     } else {
                         jc.usage();
                     }
                     System.exit(1);
                 }
             }
-            System.err.println("Parse error: " + e.getMessage());
-            jc.usage();
+            if (commandstring == null) { // user ran without any command
+                jc.usage();
+                System.exit(0);
+            }
+            System.err.println("[ERROR] " + e.getMessage());
+            System.err.println("[ERROR] your command: " + commandstring);
+            System.err.println("[ERROR] enter java -jar Lr2pg -h for more information.");
             System.exit(1);
         }
         String parsedCommand = jc.getParsedCommand();
+        if (parsedCommand == null) {
+            jc.usage(); // user ran program with no arguments, probably help is want is wanted.
+            System.exit(0);
+        }
+        if (!commandnames.contains(parsedCommand)) {
+            System.err.println("[ERROR] did not recognize command \"" + parsedCommand + "\"");
+            System.err.println("[ERROR] available commands are " + String.join(", ", commandnames));
+            System.err.println("[ERROR] enter java -jar Lr2pg -h for more information.");
+            System.exit(1);
+        }
 
-        if ( diachromatic.usageHelpRequested) {
-            if (parsedCommand==null) {
+        if (diachromatic.usageHelpRequested) {
+            if (parsedCommand == null) {
                 jc.usage();
             } else {
                 jc.usage(parsedCommand);
@@ -74,20 +94,15 @@ public class Diachromatic {
             System.exit(1);
         }
 
-        if (jc.getParsedCommand()==null ) {
+        if (jc.getParsedCommand() == null) {
             System.err.println("[ERROR] no command passed");
             jc.usage();
             System.exit(1);
         }
 
-        if ( diachromatic.usageHelpRequested) {
 
-            jc.usage();
-            System.exit(1);
-        }
-        String command = jc.getParsedCommand();
-        Command diachromaticCommand=null;
-        switch (command) {
+        Command diachromaticCommand;
+        switch (parsedCommand) {
             case "truncate":
                 diachromaticCommand = truncate;
                 break;
@@ -101,17 +116,18 @@ public class Diachromatic {
                 diachromaticCommand = summarize;
                 break;
             default:
-                throw new DiachromaticException("Did not recognize command: "+ command);
+                throw new DiachromaticException("Did not recognize command: " + parsedCommand);
         }
         try {
             diachromaticCommand.execute();
         } catch (DiachromaticException e) {
             e.printStackTrace();
         }
+
     }
 
     public static String getVersion() {
-        String version="0.0.0";// default, should be overwritten by the following.
+        String version = "0.0.0";// default, should be overwritten by the following.
         try {
             Package p = Diachromatic.class.getPackage();
             version = p.getImplementationVersion();
