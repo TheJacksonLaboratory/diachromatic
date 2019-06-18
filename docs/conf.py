ceb807778dd68a4e7ac574e786cc2b1ffa0a39e7
jtroehr <johannes.roehr@fu-berlin.de>
2019-06-18 14:57:32 +0200

    Update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ author = u'Peter Hansen, Peter Robinson'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.1'
+version = u'0.5'
 # The full version, including alpha/beta/rc tags.
 release = u'1'
 
