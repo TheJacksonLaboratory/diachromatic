696be23156ecb0487ebc88cb7877522546083ba8
hansenp <10495485+hansenp@users.noreply.github.com>
2024-02-09 16:55:31 +0000

    Deploying to gh-pages from @ TheJacksonLaboratory/diachromatic@65508462bfafa8d2ae0a768688f345747dfc01b0 🚀

--- src/main/java/org/jax/diachromatic/allelespec/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes to investigate allele specific interactions on the basis of a skew of alleles
- * in the BAM file for a capture Hi-C or related experiment. The required input is a
- * BAM file and a definition file with a list of digests, and the summarize is a list
- * of allele specificity for both digests and enriched digest pairs.
- * @author peter.robinson@jax.org
- */
-package org.jax.diachromatic.allelespec;
--- src/main/java/org/jax/diachromatic/exception/DiachromaticRuntimeException.java
+++
@@ -1,6 +0,0 @@
-package org.jax.diachromatic.exception;
-
-public class DiachromaticRuntimeException extends RuntimeException{
-    public DiachromaticRuntimeException() { super();}
-    public DiachromaticRuntimeException(String m) { super(m);}
-}
